c8c480c3e5863faca00a6cb09d54a214a1a9c640
jamesbinnerts-gif <james.binnerts@gmail.com>
2025-12-24 12:44:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function bestelProduct(productNaam, knop) {
+    const product = knop.closest(".product");
+    const kleur = product.querySelector("select").value;
+    const aantal = product.querySelector("input").value;
+
+    const telefoonNummer = "31623010458";
+
+    const bericht =
+        "Hallo! Ik wil graag bestellen:\n\n" +
+        "Product: " + productNaam + "\n" +
+        "Kleur: " + kleur + "\n" +
+        "Aantal: " + aantal;
+
+    const whatsappLink =
+        "https://wa.me/" + telefoonNummer +
+        "?text=" + encodeURIComponent(bericht);
+
+    window.open(whatsappLink, "_blank");
+}
